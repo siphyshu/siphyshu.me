fdba30c0df3782349213500b9db56788ab498295
Jaiyank Saxena <siphyshu@gmail.com>
2024-07-20 20:22:42 +0530

    Updated description

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,7 @@ import "./globals.css"
 
    export const metadata = {
      title: "siphyshu // jaiyank",
-     description: "Personal website of Jaiyank",
+     description: "Personal website of Jaiyank aka Siphyshu. 20y/o computer science student from India.",
    }
 
    export default function RootLayout({ children }) {
